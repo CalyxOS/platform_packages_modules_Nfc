a7909224d1e5b21d31af74b416ca31898e91740a
Alexandra Ducati-Manas <alexandra.ducati-manas@st.com>
2025-09-22 23:08:33 -0700

    Need reset actualNbEe for MEP case
    Bug: 446613563
    Test: compiles
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://partner-android-review.googlesource.com/q/commit:311d376b777295cd98193ca6d7489838e14a653b
    Merged-In: Ifcb77840de0ebc34e72214382bc49468bd16a898
    Change-Id: Ifcb77840de0ebc34e72214382bc49468bd16a898

--- NfcNci/nci/jni/RoutingManager.cpp
+++ NfcNci/nci/jni/RoutingManager.cpp
@@ -292,6 +292,7 @@ bool RoutingManager::isTypeATypeBTechSupportedInEe(tNFA_HANDLE eeHandle) {
   }
 
   if (mEuiccMepMode) {
+    actualNbEe = NFA_EE_MAX_EE_SUPPORTED;
     memset(&eeInfo, 0, actualNbEe * sizeof(tNFA_EE_INFO));
     nfaStat = NFA_EeGetMepInfo(&actualNbEe, eeInfo);
     if (nfaStat != NFA_STATUS_OK) {
