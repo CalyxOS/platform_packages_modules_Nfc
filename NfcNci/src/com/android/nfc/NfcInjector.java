96c53ab9d9499fd5703410b4e40b1abc2a21ec07
George Chang <georgekgchang@google.com>
2025-11-24 11:13:52 +0000

    [nfc] Default disallow TAG and TECH DISCOVERED
    Set disallow in tag intent preference when tag dispatched to
    ACTION_TAG_DISCOVERED or ACTION_TECH_DISCOVERED to the app for
    the first time.
    Start a new pref list due to users might explicitly allowed TAG intents before.
    
    Flag: com.android.nfc.module.flags.nfcstack_26q2_updates
    Bug: 389749775
    Test: atest NfcNciUnitTests
    Change-Id: I87f154eb04377e2cf8298eabb83baad213d28eed

--- NfcNci/src/com/android/nfc/NfcInjector.java
+++ NfcNci/src/com/android/nfc/NfcInjector.java
@@ -395,7 +395,8 @@ public class NfcInjector {
 
     /** Creates a NfcTagAllowNotification object */
     public NfcTagAllowNotification createNfcTagAllowNotification(
-            Context context, List<String> appNames) {
-        return new NfcTagAllowNotification(context, appNames);
+            Context context, List<String> appNames, boolean allow) {
+        return new NfcTagAllowNotification(context, appNames, allow);
     }
+
 }
