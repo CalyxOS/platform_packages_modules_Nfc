e9d7f529516c63680977cab3f11d075f1d4a990e
TYM Tsai <tymtsai@google.com>
2026-04-01 07:16:12 +0000

    Call unbindService() when bindServiceAsUser() fails
    According to bindServiceAsUser() documentation, unbindService() should
    be called even if bindServiceAsUser() returns false.
    Currently, HostEmulationManager only calls unbindService() when
    bindServiceAsUser() returns true, which might cause resource leaks.
    This change ensures unbindService() is called in all failure paths
    of bindServiceAsUser().
    
    Bug: 346784817
    Test: atest HostEmulationManagerTest
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:00cf7f69d42c3977d2b0e9c1cc2954075bea9b2a
    Merged-In: Ic45ac75147f9be0dfb239759b89e92ec4e5fbd3f
    Change-Id: Ic45ac75147f9be0dfb239759b89e92ec4e5fbd3f

--- NfcNci/src/com/android/nfc/cardemulation/HostEmulationManager.java
+++ NfcNci/src/com/android/nfc/cardemulation/HostEmulationManager.java
@@ -1296,6 +1296,7 @@ public class HostEmulationManager {
                     if (nfcHceLatencyEvents()) {
                         Trace.endAsyncSection(EVENT_HCE_BIND_SERVICE, 0);
                     }
+                    mContext.unbindService(connection);
                     Log.e(TAG, "bindServiceIfNeededLocked: Could not bind service");
                 }
             } catch (SecurityException e) {
@@ -1470,6 +1471,7 @@ public class HostEmulationManager {
                 if (nfcHceLatencyEvents()) {
                     Trace.endAsyncSection(EVENT_HCE_BIND_PAYMENT_SERVICE, 0);
                 }
+                mContext.unbindService(mPaymentConnection);
                 Log.e(TAG, "bindPaymentServiceLocked: Could not bind (persistent) "
                         + "payment service");
             }
